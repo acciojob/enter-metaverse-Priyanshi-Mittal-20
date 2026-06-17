63f2b5db870de1c2ada7593b1673eec5f99e37a4
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-06-17 12:28:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function handleClick()
+{
+	let para=document.querySelector("#status")
+	para.innerText=""
+	let h1=document.createElement("h1")
+	h1.innerText="Entered Metaverse"
+	document.body.prepend(h1)
+}
